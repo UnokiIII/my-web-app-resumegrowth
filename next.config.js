b75eb570cd7db4aa88cb7a6fb804f14ce9162a50
魏大仙的mac <apple@weidaxiandeMacBook-Pro-2.local>
2026-03-25 14:09:01 +0800

    chore: bump frontend version to 2.0.1

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_FRONTEND_VERSION: '2.0.0',
+    NEXT_PUBLIC_FRONTEND_VERSION: '2.0.1',
   },
   images: {
     domains: [],
