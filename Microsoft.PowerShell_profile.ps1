000e3e72f58779e91e68d94a969f4ad8c47fc23b
UnokiIII <unokiiii@users.noreply.github.com>
2026-03-26 14:05:00 +0800

    chore: restore handoff artifacts after Volta reorg

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,9 @@
+$utf8 = [System.Text.UTF8Encoding]::new($false)
+[Console]::InputEncoding = $utf8
+[Console]::OutputEncoding = $utf8
+$OutputEncoding = $utf8
+
+try {
+  chcp 65001 > $null
+} catch {
+}
