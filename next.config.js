eaf5c8c3a283dc9ad248e15ba34098de8e399b71
UnokiIII <unokiiii@users.noreply.github.com>
2026-03-26 14:09:33 +0800

    chore: bump frontend version to 2.2.0

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_FRONTEND_VERSION: '2.1.2',
+    NEXT_PUBLIC_FRONTEND_VERSION: '2.2.0',
   },
   images: {
     domains: [],
